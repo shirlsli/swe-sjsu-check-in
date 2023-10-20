10b4fbb6e63ce5f8ff5dab87e479a411bbe6235a
Devansh Bhargava <devansh@Devanshs-MacBook-Pro.local>
2023-10-20 14:52:12 -0700

    Update User class

--- checkin/src/main/java/org/swesjsu/checkin/models/User.java
+++ checkin/src/main/java/org/swesjsu/checkin/models/User.java
@@ -2,11 +2,15 @@ package org.swesjsu.checkin.models;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+
 @Document("users")
 public class User {
 
         private String fullName;
         private int points;
+
+        //private List<Event> events = new ArrayList<>();
         
         public User(String fullName, int points) {
             this.fullName = fullName;
@@ -28,4 +32,13 @@ public class User {
         public void setPoints(int points) {
             this.points = points;
         }
+
+        enum PermissionStatus{
+            ADMIN,
+            NATIONALSJSU,
+            SWESJSU
+        }
+
+
+
 }
