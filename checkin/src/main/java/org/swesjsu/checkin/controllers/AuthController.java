e2d9f63ae021f88c10ac33a066b4821517116079
surelyli <shirley.li@sjsu.edu>
2023-10-21 14:02:32 -0700

    Added dummy user to database

--- checkin/src/main/java/org/swesjsu/checkin/controllers/AuthController.java
+++ checkin/src/main/java/org/swesjsu/checkin/controllers/AuthController.java
@@ -1,23 +1,55 @@
 package org.swesjsu.checkin.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.swesjsu.checkin.db.User;
+import org.swesjsu.checkin.db.UserRepository;
+import org.swesjsu.checkin.services.UserService;
+
+import lombok.extern.apachecommons.CommonsLog;
+import reactor.core.publisher.Mono;
+
+import org.swesjsu.checkin.services.UserService;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+@CommonsLog
 @RestController
 @RequestMapping("/")
 public class AuthController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public Map<String, Object> currentUser(OAuth2AuthenticationToken token) {
-        return token.getPrincipal().getAttributes();
+    public Mono<Object> authenticateUser(OAuth2AuthenticationToken token) {
+        // just get a certain attribute (email), check if in database
+        // make a User object with the JSON info
+        Map<String, Object> attributes = token.getPrincipal().getAttributes();
+        String fullName = (String) attributes.get("name");
+        String email = (String) attributes.get("email");
+        User dummyUser = new User(fullName, email);
+        log.info(dummyUser.getFullName() + ", " + dummyUser.getEmail());
+        userService.addUserToDatabase(dummyUser).subscribe(user -> {
+            log.info("User saved: " + user.getFullName());
+        },
+        error -> {
+            log.error("Error while saving user: " + error.getMessage());
+        });
+        return userService.addUserToDatabase(dummyUser).map(user -> {
+            log.info("Success? " + user.getFullName() + ", " + user.getEmail());
+            return token.getPrincipal().getAttributes();
+        });
     }
 
-    // https://www.baeldung.com/spring-security-5-oauth2-login
-    // https://spring.io/guides/tutorials/spring-boot-oauth2/ "How to Add a Local User Database" section
-    
+// https://www.baeldung.com/spring-security-5-oauth2-login
+// https://spring.io/guides/tutorials/spring-boot-oauth2/ "How to Add a Local
+// User Database" section
+
+//
 }
