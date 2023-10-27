4fa061c2566cc66550d6e300c425862601e3dcf3
ckim115 <christina.hayoung@gmail.com>
2023-10-27 12:50:30 -0700

    Add event.jsx

---
+++ checkin/client/src/components/event.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { TextField } from "@mui/material"
+
+export default function Event() {
+    return (<div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>)
+}
