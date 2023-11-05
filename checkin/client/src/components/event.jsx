c1ef048457b5a110c34d7fce87b09e9a2e2138ae
ckim115 <christina.hayoung@gmail.com>
2023-11-04 22:52:43 -0700

    Create dashboard.jsx

--- checkin/client/src/components/event.jsx
+++ checkin/client/src/components/event.jsx
@@ -1,8 +1,12 @@
 import React from "react"
-import { TextField } from "@mui/material"
 
 export default function Event() {
     return (<div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <div class="headline">
+            <p>Points</p>
+            <a href="">Event Name</a>
+            <p>Date</p>
+            <p>Location</p>
+        </div>
     </div>)
 }
