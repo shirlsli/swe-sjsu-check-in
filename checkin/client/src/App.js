1cab62e1f8186c22af88cc6b52f23ec9fe6e4f01
SHU <chulie2003@gmail.com>
2024-01-12 04:02:28 -0800

    admin changes

--- checkin/client/src/App.js
+++ checkin/client/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Event from './components/event.jsx';
-import Profile from './components/profile';
+import Profile from './components/profile.jsx';
+import Admin from './components/admin.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Admin></Admin>
     </div>
   );
 }
